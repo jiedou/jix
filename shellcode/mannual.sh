44c627d60fda116b69660aed80024717dd352c91
jix <jixfly@163.com>
2017-10-29 10:34:26 +0800

    add mannual.sh

---
+++ shellcode/mannual.sh
@@ -0,0 +1,9 @@
+sudo apt-get install manpages -y
+sudo apt-get install manpages-de -y 
+sudo apt-get install manpages-de-dev -y 
+sudo apt-get install manpages-dev -y
+sudo apt-get install manpages-posix -y
+sudo apt-get install manpages-posix-de -yv
+sudo apt-get install linux-doc -y
+sudo apt-get install libcorelinux-doc -y
+
